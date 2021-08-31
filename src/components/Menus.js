0df16a35ddbada94122874b5e8d8ed11f25922c6
bobi1539 <ahmadrival833@gmail.com>
2021-08-31 21:16:55 +0700

    membuat tampilan produk

---
+++ src/components/Menus.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Col, Card } from "react-bootstrap";
+import { numberWithCommas } from "../utils/utils";
+
+const Menus = ({ menu }) => {
+  return (
+    <Col md={4} xs={6} className="mb-4">
+      <Card className="shadow">
+        <Card.Img
+          height={150}
+          variant="top"
+          src={
+            "assets/images/" +
+            menu.category.nama.toLowerCase() +
+            "/" +
+            menu.gambar
+          }
+        />
+        <Card.Body>
+          <Card.Text>
+            <strong>
+              {menu.nama} ({menu.kode}){" "}
+            </strong>
+          </Card.Text>
+          <Card.Text>Rp. {numberWithCommas(menu.harga)}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default Menus;
